1dd9727c2f3e05baa062fab5d9a15eefcd8e9b00
okeeman@gmail.com <okeeman@gmail.com@5b4a8bd0-d0a6-f753-e7ea-aaf5cd7b3fa0>
2012-11-28 21:42:54 +0000

    Console client final version

---
+++ trunk/consoleclient/HouseConditionConsoleClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Web Service.
+using HouseConditionConsoleClient.HouseCondition;
+// Required to bring WebGet and WebChannelFactory into scope. Must change from default .NET Client Framework 4 to .NET Framework 4 and add reference to System.ServiceModel.Web.
+using System.ServiceModel.Web;
+// Required to bring ServiceContract and OperationContract into scope.
+using System.ServiceModel;
+
+namespace HouseConditionConsoleClient
+{
+    [ServiceContract]
+    public interface IHouseConditionService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/")]
+        decimal? GetTotalCosts();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (HouseConditionClient c = new HouseConditionClient())
+                {
+                    Console.WriteLine("Total repair costs (SOAP): {0:C}", c.GetTotalCosts());
+                }
+
+                // Don't have to append .svc extension.
+                WebChannelFactory<IHouseConditionService> cf = new WebChannelFactory<IHouseConditionService>(new Uri("http://housingcondition.cloudapp.net/HouseCondition/pox"));
+                IHouseConditionService channel = cf.CreateChannel();
+                Console.WriteLine("Total repair costs (REST): {0:C}", channel.GetTotalCosts());
+            }
+            catch (TimeoutException te)
+            {
+                Console.WriteLine(te.Message);
+            }
+            catch (FaultException fEx)
+            {
+                Console.WriteLine("This is a FaultException:\n" + fEx.Message);
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine(ce.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
