c54bab4f8ca2aff55fc2dbec14a0c4fe21e24d0f
okeeman@gmail.com <okeeman@gmail.com@5b4a8bd0-d0a6-f753-e7ea-aaf5cd7b3fa0>
2012-11-28 21:00:46 +0000

    MVC Web app final version

---
+++ mvcwebapp/HouseRepairMVC.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HouseRepairMVC;
+using HouseRepairMVC.Controllers;
+
+namespace HouseRepairMVC.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            // Redirects to HomeConditionController, Index Action so View Result of Home/Index is null.
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestHomeIndexRedirect()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as RedirectToRouteResult;
+
+            // Assert
+            // Redirects to HomeConditionController, Index Action.
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ mvcwebapp/HouseRepairMVC/Models/HouseConditionDomainService.metadata.cs
@@ -0,0 +1,50 @@
+﻿
+namespace HouseRepairMVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // The MetadataTypeAttribute identifies HouseMetadata as the class
+    // that carries additional metadata for the House class.
+    [MetadataTypeAttribute(typeof(House.HouseMetadata))]
+    public partial class House
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the House class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class HouseMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private HouseMetadata()
+            {
+            }
+
+            [Required(ErrorMessage = "Property address must be supplied")]
+            public string Address { get; set; }
+
+            // Assuming a situation where costs cannot exceed €200,000.
+            [Range(0, 200000, ErrorMessage = "Cost is greater than that allowed")]
+            public Nullable<decimal> Cost { get; set; }
+
+            public int ID { get; set; }
+
+            [Required]
+            [Display(Name = "Repairs required?")]
+            public bool Repairs { get; set; }
+        }
+    }
+}
