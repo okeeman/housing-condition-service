96d14c7c8f9519b9e3876afe97666d72505f69dc
okeeman@gmail.com <okeeman@gmail.com@5b4a8bd0-d0a6-f753-e7ea-aaf5cd7b3fa0>
2012-11-28 20:58:30 +0000

    Phone app final version

---
+++ trunk/phoneapp/HouseConditionPhoneAppTests/HouseConditionPhoneAppUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HouseConditionPhoneAppTests
+{
+    [TestClass]
+    public class HouseConditionPhoneAppUnitTest
+    {
+        [TestMethod]
+        public void TestgetTotalCostsSOAP()
+        {
+        }
+
+        [TestMethod]
+        public void TestgetTotalCostsREST()
+        {
+        }
+    }
+}
---
+++ trunk/phoneapp/HousingConditionPhoneApp/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+// Web Service.
+using HousingConditionPhoneApp.HouseCondition;
+// Required to bring ServiceContract and OperationContract into scope.
+using System.ServiceModel;
+// Required to bring StreamReader into scope.
+using System.IO;
+
+
+namespace HousingConditionPhoneApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void getTotalCostsSOAP_Click(object sender, RoutedEventArgs e)
+        {
+            HouseConditionClient c = new HouseConditionClient();
+            c.GetTotalCostsAsync();
+            c.GetTotalCostsCompleted += new EventHandler<HouseCondition.GetTotalCostsCompletedEventArgs>(c_getTotalCostsCompleted);
+        }
+
+        private void c_getTotalCostsCompleted(object sender, HouseCondition.GetTotalCostsCompletedEventArgs e)
+        {
+            textBlock1.Text = e.Result.ToString();
+        }
+
+        private void getTotalCostsREST_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                Uri uri = new Uri("http://housingcondition.cloudapp.net/housecondition/pox");
+                wc.OpenReadCompleted += new OpenReadCompletedEventHandler(wc_OpenReadCompleted);
+                wc.OpenReadAsync(uri);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void wc_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(e.Result);
+                MessageBox.Show(reader.ReadToEnd().ToString());
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
